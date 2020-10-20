97e109c2b40bafac041bc634fa21537b6dc6d1e3
ToBeHappy <45383854+yangkanggit@users.noreply.github.com>
2020-10-20 10:59:00 +0800

    Create FileStatusHelper.cs

---
+++ FileStatusHelper.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffCountByBeyond
+{
+    public class FileStatusHelper
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr _lopen(string lpPathName, int iReadWrite);
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
+        public const int OF_READWRITE = 2;
+        public const int OF_SHARE_DENY_NONE = 0x40;
+        public static readonly IntPtr HFILE_ERROR = new IntPtr(-1);
+        /// <summary>
+        /// 查看文件是否被占用
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static bool IsFileOccupied(string filePath)
+        {
+            IntPtr vHandle = _lopen(filePath, OF_READWRITE | OF_SHARE_DENY_NONE);
+            CloseHandle(vHandle);
+            return vHandle == HFILE_ERROR ? true : false;
+        }
+    }
+}
